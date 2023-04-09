fb033eb63977a6f4363439f38098efbd39629ad8
Arjun Nair <arjunnair.mec@gmail.com>
2023-04-09 04:43:14 +0200

    Update links to work relatively

--- js/pwa-handler.js
+++ js/pwa-handler.js
@@ -4,7 +4,7 @@ const serviceWorker = navigator.serviceWorker;
 // Register our ServiceWorker script, if serviceWorker is available.
 if (serviceWorker) {
     serviceWorker
-        .register("/service-worker.js")
+        .register("service-worker.js")
         .then(() => console.log("ServiceWorker Registered to the Application!"))
         .catch(() => console.log("Failed to Register the ServiceWorker."));
 }
