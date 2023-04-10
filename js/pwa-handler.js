8f27308625bd4d09a410c2ddae01545655b66ff7
Arjun Nair <arjunnair.mec@gmail.com>
2023-04-10 02:55:39 +0200

    Add additional info to PWA install. Remove unwanted stuff for PWA

--- js/pwa-handler.js
+++ js/pwa-handler.js
@@ -1,19 +1,9 @@
-// Reference the serviceWorker.
-const serviceWorker = navigator.serviceWorker;
-
-// Register our ServiceWorker script, if serviceWorker is available.
-if (serviceWorker) {
-    serviceWorker
-        .register("service-worker.js")
-        .then(() => console.log("ServiceWorker Registered to the Application!"))
-        .catch(() => console.log("Failed to Register the ServiceWorker."));
-}
-
 // Create a variable to defer the beforeinstallprompt event.
 let beforeInstallEvent;
 
 // Reference the install button from DOM.
 const installButton = document.getElementById("install");
+const installHelper = document.getElementById("install-helper");
 
 // Watch for the beforeinstallevent and defer it.
 window.addEventListener("beforeinstallprompt", (event) => {
@@ -24,6 +14,11 @@ window.addEventListener("beforeinstallprompt", (event) => {
 
 // Prompt for Installation when install button is clicked.
 installButton.addEventListener("click", () => {
+    if (beforeInstallEvent == undefined) {
+        installButton.disabled = false;
+        installHelper.classList.remove("d-none");
+        return;
+    }
     beforeInstallEvent
         .prompt()
         .then((choice) => {
